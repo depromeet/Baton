d1767912f29ca4a9ae881f3c0288236bf4d0e545
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-04-25 16:38:23 +0900

    home, search, chatting, mypage Fragment 추가

---
+++ app/src/main/java/com/depromeet/baton/presentation/ui/search/SearchFragment.kt
@@ -0,0 +1,18 @@
+package com.depromeet.baton.presentation.ui.search
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.depromeet.baton.R
+import com.depromeet.baton.databinding.FragmentMyPageBinding
+import com.depromeet.baton.databinding.FragmentSearchBinding
+import com.depromeet.baton.presentation.base.BaseFragment
+
+class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
