0d7b234a03cf38a90ec9557dfa1c81eca70c7780
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-04-25 12:46:30 +0900

    MainActivity, SampleActivity에 BaseActivity 반영

--- app/src/main/java/com/depromeet/baton/presentation/main/MainActivity.kt
+++ app/src/main/java/com/depromeet/baton/presentation/main/MainActivity.kt
@@ -1,17 +1,15 @@
 package com.depromeet.baton.presentation.main
 
 import android.os.Bundle
+import com.depromeet.baton.R
 import com.depromeet.baton.databinding.ActivityMainBinding
 import com.depromeet.baton.presentation.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : BaseActivity() {
-
-    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/depromeet/baton/presentation/sample/SampleActivity.kt
+++ app/src/main/java/com/depromeet/baton/presentation/sample/SampleActivity.kt
@@ -2,20 +2,17 @@ package com.depromeet.baton.presentation.sample
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import com.depromeet.baton.R
 import com.depromeet.baton.databinding.ActivitySampleBinding
 import com.depromeet.baton.presentation.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SampleActivity : BaseActivity() {
+class SampleActivity : BaseActivity<ActivitySampleBinding>(R.layout.activity_sample) {
 
-    private val binding: ActivitySampleBinding by lazy {
-        ActivitySampleBinding.inflate(layoutInflater)
-    }
     private val viewModel: SampleViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
     }
 }
