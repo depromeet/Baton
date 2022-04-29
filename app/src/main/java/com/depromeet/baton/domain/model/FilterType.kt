cbe60be0503033c42f40ba70733f23f23bc3d7ec
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-04-29 16:52:57 +0900

    Price, Term enum class 추가

---
+++ app/src/main/java/com/depromeet/baton/domain/model/FilterType.kt
@@ -0,0 +1,11 @@
+package com.depromeet.baton.domain.model
+
+enum class FilterType {
+    Alignment,
+    TicketKind,
+    Term,
+    Price,
+    TransactionMethod,
+    AdditionalOptions,
+    HashTag,
+}
