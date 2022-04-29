f951ef6cb0039a0834f2eb5a48e26fbbd1a29460
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-04-29 16:52:13 +0900

    BottomSheet에 들어가는 Filter Fragment 생성

---
+++ app/src/main/java/com/depromeet/baton/presentation/ui/filter/view/HashTagFragment.kt
@@ -0,0 +1,16 @@
+package com.depromeet.baton.presentation.ui.filter.view
+
+import android.os.Bundle
+import android.view.View
+import com.depromeet.baton.R
+import com.depromeet.baton.databinding.FragmentHashTagBinding
+import com.depromeet.baton.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HashTagFragment : BaseFragment<FragmentHashTagBinding>(R.layout.fragment_hash_tag) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
