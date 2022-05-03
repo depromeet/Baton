a60c034addeade6838c2f5473a54e5e08fcf1aa8
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-05-04 01:08:09 +0900

    사용안하는 FilterUtil 삭제

--- app/src/main/java/com/depromeet/baton/presentation/ui/filter/FilterUtil.kt
+++ app/src/main/java/com/depromeet/baton/presentation/ui/filter/FilterUtil.kt
@@ -1,9 +1,9 @@
 package com.depromeet.baton.presentation.ui.filter
 
 import com.depromeet.baton.R
-import com.depromeet.baton.domain.model.FilterType
 import com.depromeet.baton.domain.model.TicketKind
 
+//bindingAdapter에서 사용
 fun getTicketKindFromCheckedId(checkedId: Int): TicketKind? {
     return when (checkedId) {
         R.id.chip_ticket_kind_gym -> TicketKind.GYM
@@ -14,27 +14,3 @@ fun getTicketKindFromCheckedId(checkedId: Int): TicketKind? {
     }
 }
 
-fun getFilterTypeFromPosition(position: Int): FilterType? {
-    return when (position) {
-        0 -> FilterType.Alignment
-        1 -> FilterType.TicketKind
-        2 -> FilterType.Term
-        3 -> FilterType.Price
-        4 -> FilterType.TransactionMethod
-        5 -> FilterType.AdditionalOptions
-        6 -> FilterType.HashTag
-        else -> null
-    }
-}
-
-fun getPositionFromFilterType(filterType: FilterType): Int {
-    return when (filterType) {
-        FilterType.Alignment -> 0
-        FilterType.TicketKind -> 1
-        FilterType.Term -> 2
-        FilterType.Price -> 3
-        FilterType.TransactionMethod -> 4
-        FilterType.AdditionalOptions -> 5
-        FilterType.HashTag -> 6
-    }
-}
