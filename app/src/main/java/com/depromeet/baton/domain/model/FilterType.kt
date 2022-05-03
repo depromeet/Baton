f28107e146f4a89ef2fb142ede427e52fc4b91e8
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-05-04 00:58:19 +0900

    FilterType value추가

--- app/src/main/java/com/depromeet/baton/domain/model/FilterType.kt
+++ app/src/main/java/com/depromeet/baton/domain/model/FilterType.kt
@@ -1,11 +1,13 @@
 package com.depromeet.baton.domain.model
 
-enum class FilterType {
-    Alignment,
-    TicketKind,
-    Term,
-    Price,
-    TransactionMethod,
-    AdditionalOptions,
-    HashTag,
+import com.depromeet.baton.R
+
+enum class FilterType(var value: String) {
+    Alignment("정렬"),
+    TicketKind("양도권 종류"),
+    Term("기간"),
+    Price("가격"),
+    TransactionMethod("거래방법"),
+    AdditionalOptions("추가옵션"),
+    HashTag("해시태그"),
 }
