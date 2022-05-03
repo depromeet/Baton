f657c28617927b35a3054481a03c521b9e7e23c5
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-05-04 01:17:35 +0900

    HomeFragment에서 FilterChipFragment사용

--- app/src/main/java/com/depromeet/baton/presentation/ui/home/view/HomeFragment.kt
+++ app/src/main/java/com/depromeet/baton/presentation/ui/home/view/HomeFragment.kt
@@ -2,39 +2,15 @@ package com.depromeet.baton.presentation.ui.home.view
 
 import android.os.Bundle
 import android.view.View
-import androidx.core.os.bundleOf
-import androidx.fragment.app.activityViewModels
 import com.depromeet.baton.R
 import com.depromeet.baton.databinding.FragmentHomeBinding
-import com.depromeet.baton.domain.model.FilterType
 import com.depromeet.baton.presentation.base.BaseFragment
-import com.depromeet.baton.presentation.ui.filter.getPositionFromFilterType
-import com.depromeet.baton.presentation.ui.filter.view.BottomFilterFragment
-import com.depromeet.baton.presentation.ui.filter.viewmodel.FilterViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
-    private val filterViewModel: FilterViewModel by activityViewModels()
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.homeFragment = this
-        binding.filterViewModel = filterViewModel
-    }
-
-    fun setFilterChipClickListener(filterType: FilterType) {
-        val bottomFilterFragment = BottomFilterFragment().apply {
-            arguments = bundleOf(CHECKED_FILTER_POSITION_KEY to getPositionFromFilterType(filterType))
-        }
-        bottomFilterFragment.show(
-            childFragmentManager,
-            bottomFilterFragment.tag
-        )
-    }
-
-    companion object {
-        const val CHECKED_FILTER_POSITION_KEY = "com.depromeet.baton.presentation.ui.home.view CHECKED_FILTER_POSITION_KEY"
     }
 }
