e12c040e35a25122c0da1d2e3c3a277e6bb242fd
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-05-04 01:20:27 +0900

    Bds text, isSelected 세팅을 위한 BindingAdapter 추가 및 setFilterTypeOnCheckedChanged삭제

--- app/src/main/java/com/depromeet/baton/presentation/util/BindingAdapters.kt
+++ app/src/main/java/com/depromeet/baton/presentation/util/BindingAdapters.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModel
 import com.depromeet.baton.domain.model.FilterType
 import com.depromeet.baton.presentation.ui.filter.getTicketKindFromCheckedId
 import com.depromeet.baton.presentation.ui.filter.viewmodel.FilterViewModel
-import com.depromeet.bds.R
+import com.depromeet.bds.component.BdsFilter
 import com.google.android.material.chip.Chip
 
 @BindingAdapter("viewModel", "itemType")
@@ -20,16 +20,8 @@ fun setFilterItemOnCheckedChanged(view: Chip, viewModel: ViewModel, itemType: Fi
     }
 }
 
-@BindingAdapter("viewModel", "filterType")
-fun setFilterTypeOnCheckedChanged(view: Chip, viewModel: ViewModel, filterType: FilterType) {
-    view.setOnCheckedChangeListener { _, _ ->
-        when (filterType) {
-            FilterType.TicketKind -> {
-                view.isChecked = (viewModel as? FilterViewModel)?.isTicketKindFiltered?.value ?: false
-            }
-        }
-        //chip의 closeIcon 색상변경 반영이 느려서 여기서 색상변경 코드를 한번 더 작성해주어야하는 문제가,,
-        if (view.isChecked) view.setCloseIconTintResource(R.color.blue50)
-        else view.setCloseIconTintResource(R.color.gy60)
-    }
+@BindingAdapter("bds_text", "isSelected")
+fun setBdsFilter(view: BdsFilter, text: String, isSelected: Boolean) {
+    view.text = text
+    view.isSelected = isSelected
 }
