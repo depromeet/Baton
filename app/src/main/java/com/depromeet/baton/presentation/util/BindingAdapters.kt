8bceb9c2b3ad475f1d7676f06e51bbf50e0a18ca
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-05-04 14:16:52 +0900

    BdsChoice Chip으로 반영

--- app/src/main/java/com/depromeet/baton/presentation/util/BindingAdapters.kt
+++ app/src/main/java/com/depromeet/baton/presentation/util/BindingAdapters.kt
@@ -1,27 +1,17 @@
 package com.depromeet.baton.presentation.util
 
 import androidx.databinding.BindingAdapter
-import androidx.lifecycle.ViewModel
-import com.depromeet.baton.domain.model.FilterType
-import com.depromeet.baton.presentation.ui.filter.getTicketKindFromCheckedId
-import com.depromeet.baton.presentation.ui.filter.viewmodel.FilterViewModel
+import com.depromeet.bds.component.BdsChoiceChip
 import com.depromeet.bds.component.BdsFilter
-import com.google.android.material.chip.Chip
-
-@BindingAdapter("viewModel", "itemType")
-fun setFilterItemOnCheckedChanged(view: Chip, viewModel: ViewModel, itemType: FilterType) {
-    view.setOnCheckedChangeListener { _, isChecked ->
-        when (itemType) {
-            FilterType.TicketKind -> {
-                (viewModel as? FilterViewModel)
-                    ?.setTicketKind(getTicketKindFromCheckedId(view.id) ?: return@setOnCheckedChangeListener, isChecked)
-            }
-        }
-    }
-}
 
 @BindingAdapter("bds_text", "isSelected")
 fun setBdsFilter(view: BdsFilter, text: String, isSelected: Boolean) {
     view.text = text
     view.isSelected = isSelected
+}
+
+@BindingAdapter("isOn", "shape")
+fun setBdsChoice(view: BdsChoiceChip, isOn: Boolean, shape: String) {
+    if (shape == "outlined") view.setOnAndShape(isOn, 0)
+    else view.setOnAndShape(isOn, 1)
 }
