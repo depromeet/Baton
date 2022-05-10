6e3c9edf47f705fc91014b2f8a7b90072bef2071
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-05-10 15:50:27 +0900

    filterViewModel 각 칩종류, 선택여부 Map으로 관리

--- app/src/main/java/com/depromeet/baton/presentation/ui/filter/viewmodel/FilterViewModel.kt
+++ app/src/main/java/com/depromeet/baton/presentation/ui/filter/viewmodel/FilterViewModel.kt
@@ -2,24 +2,22 @@ package com.depromeet.baton.presentation.ui.filter.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.depromeet.baton.domain.model.FilterType
-import com.depromeet.baton.domain.model.TicketKind
+import com.depromeet.baton.domain.model.*
 import com.depromeet.baton.presentation.base.BaseViewModel
 import com.depromeet.baton.presentation.util.ListLiveData
-import com.depromeet.baton.presentation.util.ResourcesProvider
+import com.depromeet.baton.presentation.util.MapListLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class FilterViewModel @Inject constructor(
-    private val resourcesProvider: ResourcesProvider
 ) : BaseViewModel() {
 
     private val _allCount = MutableLiveData<Int>(20)
     val allCount: LiveData<Int> = _allCount
 
     /*양도권 종류*/
-    private val _ticketKindList = ListLiveData<String>()
+    var ticketKindCheckedList = MapListLiveData<TicketKind, Boolean>()
 
     private val _isTicketKindFiltered = MutableLiveData<Boolean>()
 
@@ -35,34 +33,115 @@ class FilterViewModel @Inject constructor(
     private val _isEtcChecked = MutableLiveData(false)
     val isEtcChecked: LiveData<Boolean> = _isEtcChecked
 
+    /*거래방법*/
+    var transactionMethodCheckedList = MapListLiveData<TransactionMethod, Boolean>()
+
+    private val _isTransactionMethodFiltered = MutableLiveData<Boolean>()
+
+    private val _isFaceChecked = MutableLiveData(false)
+    val isFaceChecked: LiveData<Boolean> = _isFaceChecked
+
+    private val _isNonFaceChecked = MutableLiveData(false)
+    val isNonFaceChecked: LiveData<Boolean> = _isNonFaceChecked
+
+    private val _isSellerChecked = MutableLiveData(false)
+    val isSellerChecked: LiveData<Boolean> = _isSellerChecked
+
+    private val _isConsumerChecked = MutableLiveData(false)
+    val isConsumerChecked: LiveData<Boolean> = _isConsumerChecked
+
+    /*추가옵션*/
+    private var additionalOptionsCheckedList = MapListLiveData<AdditionalOptions, Boolean>()
+
+    private val _isAdditionalOptionsFiltered = MutableLiveData<Boolean>()
+
+    private val _isShowerRoomChecked = MutableLiveData(false)
+    val isShowerRoomChecked: LiveData<Boolean> = _isShowerRoomChecked
+
+    private val _isLockerRoomChecked = MutableLiveData(false)
+    val isLockerRoomChecked: LiveData<Boolean> = _isLockerRoomChecked
+
+    private val _isSportWearrChecked = MutableLiveData(false)
+    val isSportWearrChecked: LiveData<Boolean> = _isSportWearrChecked
+
+    private val _isGxChecked = MutableLiveData(false)
+    val isGxChecked: LiveData<Boolean> = _isGxChecked
+
+    private val _isReTransferChecked = MutableLiveData(false)
+    val isReTransferChecked: LiveData<Boolean> = _isReTransferChecked
+
+    private val _isRefundChecked = MutableLiveData(false)
+    val isRefundChecked: LiveData<Boolean> = _isRefundChecked
+
+    private val _isHoldingChecked = MutableLiveData(false)
+    val isHoldingChecked: LiveData<Boolean> = _isHoldingChecked
+
+    private val _isBargainingChecked = MutableLiveData(false)
+    val isBargainingChecked: LiveData<Boolean> = _isBargainingChecked
+
+    /*해시태그*/
+    private var hashTagCheckedList = MapListLiveData<HashTag, Boolean>()
+
+    private val _isHashTagFiltered = MutableLiveData<Boolean>()
+
+    private val _isKindTeacherChecked = MutableLiveData(false)
+    val isKindTeacherChecked: LiveData<Boolean> = _isKindTeacherChecked
+
+    private val _isSystematicLessonChecked = MutableLiveData(false)
+    val isSystematicLessonChecked: LiveData<Boolean> = _isSystematicLessonChecked
+
+    private val _isCustomizedCareChecked = MutableLiveData(false)
+    val isCustomizedCareChecked: LiveData<Boolean> = _isCustomizedCareChecked
+
+    private val _isWideFacilityChecked = MutableLiveData(false)
+    val isWideFacilityChecked: LiveData<Boolean> = _isWideFacilityChecked
+
+    private val _isVariousInstrumentsChecked = MutableLiveData(false)
+    val isVariousInstrumentsChecked: LiveData<Boolean> = _isVariousInstrumentsChecked
+
+    private val _isPleasantEnvironmentChecked = MutableLiveData(false)
+    val isPleasantEnvironmentChecked: LiveData<Boolean> = _isPleasantEnvironmentChecked
+
+    private val _isQuietAtmosphereChecked = MutableLiveData(false)
+    val isQuietAtmosphereChecked: LiveData<Boolean> = _isQuietAtmosphereChecked
+
     /*position, list관련*/
+    //필터타입 순서 리스트
     val filterTypeOrderList = ListLiveData<String>()
 
+    //어디서 눌러서 왔는지
     private val _currentFilterPosition = MutableLiveData<Int>()
     val currentFilterPosition: LiveData<Int> = _currentFilterPosition
 
-    private val _currentFilterBottomPosition = MutableLiveData<Int>()
-
+    //필터 순서 총괄
     private val _filterChipList = MutableLiveData<MutableList<Pair<String, Boolean>>>()
     val filterChipList: LiveData<MutableList<Pair<String, Boolean>>> = _filterChipList
 
+    //항목상관없이 필터링된 필터들을 담는 리스트
+    private val _filteredChipList = ListLiveData<Any>()
+    val filteredChipList: ListLiveData<Any> = _filteredChipList
+
+    //필터, 검색 가능여부
+    private val _isResetAndSearchValid = MutableLiveData(false)
+    val isResetAndSearchValid: LiveData<Boolean> = _isResetAndSearchValid
+
     init {
-        //초기 필터 리스트 상태 세팅
         setFilterPosition()
         //바텀 탭레이아웃에게 그려야하는 필터순서 리스트로 전달하기 위함
         filterTypeOrderList.value = filterChipList.value?.toMap()?.keys?.toMutableList()
     }
 
-    private fun setFilterPosition() {
+    //홈에서 초기화누르거나 검색눌렀을때 초기화되어야함
+    fun setFilterPosition() {
         //필터 순서, 필터 선택여부 동적관리 -> 여기서만 관리해주도록
         _filterChipList.value = mutableListOf(
-            FilterType.Alignment.value to false,
             FilterType.TicketKind.value to (_isTicketKindFiltered.value ?: false),
             FilterType.Term.value to false,
             FilterType.Price.value to false,
-            FilterType.TransactionMethod.value to false,
-            FilterType.AdditionalOptions.value to false,
-            FilterType.HashTag.value to false,
+            FilterType.TransactionMethod.value to (_isTransactionMethodFiltered.value ?: false),
+            FilterType.AdditionalOptions.value to (_isAdditionalOptionsFiltered.value ?: false),
+            FilterType.HashTag.value to (_isHashTagFiltered.value ?: false),
+            FilterType.Alignment.value to false,
         )
     }
 
@@ -71,45 +150,116 @@ class FilterViewModel @Inject constructor(
         _currentFilterPosition.value = currentFilterPosition
     }
 
-    //홈에서 클릭해서 올 때 포지션, 바텀에서 초기화 누르는 탭 포지션이 다를 수 있음
-    //같은 position 데이터 쓰면 홈에서 클릭해서 올 때 포지션 observe하는 부분도 변해서 따로 관리
-    fun setCurrentFilterBottomPosition(currentFilterBottomPosition: Int) {
-        _currentFilterBottomPosition.value = currentFilterBottomPosition
-    }
-
-    //바텀 포지션에 따라서 필터 초기화, position이 바뀌어도 Type으로 판단하도록
     fun filterReset() {
-        val currentPositionType = filterChipList.value?.get(_currentFilterBottomPosition.value ?: return)?.first
-        when (currentPositionType) {
-            FilterType.TicketKind.value -> {
-                _isGymChecked.value = false
-                _isPtChecked.value = false
-                _isPilatesYogaChecked.value = false
-                _isEtcChecked.value = false
-                _ticketKindList.clear()
-                setIsTicketKindFiltered()
-            }
-        }
+        hashTagCheckedList.value?.clear()
+        ticketKindCheckedList.value?.clear()
+        transactionMethodCheckedList.value?.clear()
+        additionalOptionsCheckedList.value?.clear()
+        _filteredChipList.value?.clear()
+        _filteredChipList.value = _filteredChipList.value
+
+        updateChoiceChipCheckedStatus()  //모든 초이스칩 초기화
+        updateFilterChipCheckedStatus() //각 필터가 선택됐는지를 갱신
+        setFilterPosition()  //홈에서 바로 초기화 누르는 경우가 있음
+        updateResetAndSearchValid() //리셋버튼 갱신
     }
 
-    //양도권종류 아이템 클릭여부를 관리
-    fun setTicketKind(ticket: TicketKind, isChecked: Boolean) {
-        when (ticket) {
-            TicketKind.GYM -> _isGymChecked.value = isChecked
-            TicketKind.PT -> _isPtChecked.value = isChecked
-            TicketKind.PILATES_YOGA -> _isPilatesYogaChecked.value = isChecked
-            TicketKind.ETC -> _isEtcChecked.value = isChecked
-        }
-
-        if (isChecked) _ticketKindList.add(resourcesProvider.getString(ticket.value))
-        else _ticketKindList.remove(resourcesProvider.getString(ticket.value))
-
-        setIsTicketKindFiltered()
+    private fun updateChipToFilteredChipList(chip: Any, isChecked: Boolean) {
+        if (isChecked) _filteredChipList.value?.add(chip)
+        else _filteredChipList.value?.remove(chip)
+        _filteredChipList.value = _filteredChipList.value
+        updateResetAndSearchValid()
     }
 
-    //양도권종류 하나라도 선택되었는지 여부->홈에서 양도권 종류 필터링 여부
-    private fun setIsTicketKindFiltered() {
-        _isTicketKindFiltered.value = _ticketKindList.isListNotEmpty()
-        setFilterPosition()
+    //초기화, 검색 valid 처리
+    private fun updateResetAndSearchValid() {
+        _isResetAndSearchValid.value = !_filteredChipList.value.isNullOrEmpty()
+    }
+
+    /* ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ각 하위 항목 관리ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ */
+    fun setTicketKind(ticket: TicketKind, isChecked: Boolean = false) {
+        ticketKindCheckedList.setChipState(ticket, isChecked)
+        setChipSatus(ticket, isChecked)
+    }
+
+    fun setTransactionMethod(method: TransactionMethod, isChecked: Boolean = false) {
+        transactionMethodCheckedList.setChipState(method, isChecked)
+        setChipSatus(method, isChecked)
+    }
+
+    fun setAdditionalOptions(option: AdditionalOptions, isChecked: Boolean = false) {
+        additionalOptionsCheckedList.setChipState(option, isChecked)
+        setChipSatus(option, isChecked)
+    }
+
+    fun setHashTag(option: HashTag, isChecked: Boolean = false) {
+        if (isChecked && hashTagCheckedList.value?.filter { it.value }?.size == 3) return
+        hashTagCheckedList.setChipState(option, isChecked)
+        setChipSatus(option, isChecked)
+    }
+
+    private fun setChipSatus(type: Any, isChecked: Boolean) {
+        updateChoiceChipCheckedStatus()
+        updateFilterChipCheckedStatus()
+        updateChipToFilteredChipList(type, isChecked)
+    }
+
+    //선택바뀔떄마다 해당 칩 하나라도 선택됐는지
+    private fun updateFilterChipCheckedStatus() {
+        _isTicketKindFiltered.value = ticketKindCheckedList.value?.containsValue(true)
+        _isTransactionMethodFiltered.value = transactionMethodCheckedList.value?.containsValue(true)
+        _isAdditionalOptionsFiltered.value = additionalOptionsCheckedList.value?.containsValue(true)
+        _isHashTagFiltered.value = hashTagCheckedList.value?.containsValue(true)
+    }
+
+    private fun updateChoiceChipCheckedStatus() {
+
+        //양도권종류
+        _isGymChecked.value = choiceChipStatus(TicketKind.GYM)
+        _isPtChecked.value = choiceChipStatus(TicketKind.PT)
+        _isPilatesYogaChecked.value = choiceChipStatus(TicketKind.PILATES_YOGA)
+        _isEtcChecked.value = choiceChipStatus(TicketKind.ETC)
+
+        //거래방법
+        _isFaceChecked.value = choiceChipStatus(TransactionMethod.FACE)
+        _isNonFaceChecked.value = choiceChipStatus(TransactionMethod.NON_FACE)
+        _isSellerChecked.value = choiceChipStatus(TransactionMethod.SELLER)
+        _isConsumerChecked.value = choiceChipStatus(TransactionMethod.CONSUMER)
+
+        //추가옵션
+        _isShowerRoomChecked.value = choiceChipStatus(AdditionalOptions.SHOWER_ROOM)
+        _isLockerRoomChecked.value = choiceChipStatus(AdditionalOptions.LOCKER_ROOM)
+        _isSportWearrChecked.value = choiceChipStatus(AdditionalOptions.SPORT_WEAR)
+        _isGxChecked.value = choiceChipStatus(AdditionalOptions.GX)
+        _isReTransferChecked.value = choiceChipStatus(AdditionalOptions.RE_TRANSFER)
+        _isRefundChecked.value = choiceChipStatus(AdditionalOptions.REFUND)
+        _isHoldingChecked.value = choiceChipStatus(AdditionalOptions.HOLDING)
+        _isBargainingChecked.value = choiceChipStatus(AdditionalOptions.BARGAINING)
+
+        //해시태그
+        _isKindTeacherChecked.value = choiceChipStatus(HashTag.KIND_TEACHER)
+        _isSystematicLessonChecked.value = choiceChipStatus(HashTag.SYSTEMATIC_LESSON)
+        _isCustomizedCareChecked.value = choiceChipStatus(HashTag.CUSTOMIZED_CARE)
+        _isWideFacilityChecked.value = choiceChipStatus(HashTag.WIDE_FACILITY)
+        _isVariousInstrumentsChecked.value = choiceChipStatus(HashTag.VARIOUS_INSTRUMENTS)
+        _isPleasantEnvironmentChecked.value = choiceChipStatus(HashTag.PLEASANT_ENVIRONMENT)
+        _isQuietAtmosphereChecked.value = choiceChipStatus(HashTag.QUIET_ATMOSPHERE)
+    }
+
+    private fun choiceChipStatus(type: Any?): Boolean? {
+        return when (type) {
+            is TicketKind -> if (ticketKindCheckedList.value?.containsKey(type)!!)
+                ticketKindCheckedList.value?.getValue(type) else false
+
+            is TransactionMethod -> if (transactionMethodCheckedList.value?.containsKey(type)!!)
+                transactionMethodCheckedList.value?.getValue(type) else false
+
+            is AdditionalOptions -> if (additionalOptionsCheckedList.value?.containsKey(type)!!)
+                additionalOptionsCheckedList.value?.getValue(type) else false
+
+            is HashTag -> if (hashTagCheckedList.value?.containsKey(type)!!)
+                hashTagCheckedList.value?.getValue(type) else false
+            else -> null
+        }
     }
 }
