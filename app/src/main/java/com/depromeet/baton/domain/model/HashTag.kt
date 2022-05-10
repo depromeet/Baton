2f470ccb9010f9f4d08ae9e1e6410d3e7337c17c
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-05-10 15:34:09 +0900

    HashTag Enum class value 변경

--- app/src/main/java/com/depromeet/baton/domain/model/HashTag.kt
+++ app/src/main/java/com/depromeet/baton/domain/model/HashTag.kt
@@ -1,11 +1,11 @@
 package com.depromeet.baton.domain.model
 
 enum class HashTag(val value: String) {
-    KIND_TEACHER("&#35;친절한 선생님"),
-    SYSTEMATIC_LESSON("&#35;체계적인 수업"),
-    CUSTOMIZED_CARE("&#35;맞춤 케어"),
-    WIDE_FACILITY("&#35;넓은 시설"),
-    VARIOUS_INSTRUMENTS("&#35;다양한 기구"),
-    PLEASANT_ENVIRONMENT("&#35;쾌적한 환경"),
-    QUIET_ATMOSPHERE("&#35;조용한 분위기"),
+    KIND_TEACHER("#친절한 선생님"),
+    SYSTEMATIC_LESSON("#체계적인 수업"),
+    CUSTOMIZED_CARE("#맞춤 케어"),
+    WIDE_FACILITY("#넓은 시설"),
+    VARIOUS_INSTRUMENTS("#다양한 기구"),
+    PLEASANT_ENVIRONMENT("#쾌적한 환경"),
+    QUIET_ATMOSPHERE("#조용한 분위기"),
 }
