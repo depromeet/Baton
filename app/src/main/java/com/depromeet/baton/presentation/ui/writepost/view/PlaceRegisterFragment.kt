c00c06b16ce5ed5dd2a96a9bdf10a8ddd8700264
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-05-13 02:15:09 +0900

    장소등록 디자인 변경사항 반영 및 로직 수정

--- app/src/main/java/com/depromeet/baton/presentation/ui/writepost/view/PlaceRegisterFragment.kt
+++ app/src/main/java/com/depromeet/baton/presentation/ui/writepost/view/PlaceRegisterFragment.kt
@@ -3,59 +3,59 @@ package com.depromeet.baton.presentation.ui.writepost.view
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
-import android.graphics.Color
-import android.net.Uri
 import android.os.Bundle
 import android.text.Editable
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
-import com.bumptech.glide.Glide
 import com.depromeet.baton.R
 import com.depromeet.baton.databinding.FragmentPlaceRegisterBinding
 import com.depromeet.baton.presentation.base.BaseFragment
-import com.depromeet.baton.presentation.ui.address.SearchShopRvAdapter
 import com.depromeet.baton.presentation.ui.writepost.adapter.PhotoRvAdapter
 import com.depromeet.baton.presentation.ui.writepost.viewmodel.WritePostViewModel
+import com.depromeet.baton.presentation.util.shortToast
 import com.nguyenhoanglam.imagepicker.model.ImagePickerConfig
-import com.nguyenhoanglam.imagepicker.ui.camera.CameraModule
 import com.nguyenhoanglam.imagepicker.ui.imagepicker.registerImagePicker
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 
 @AndroidEntryPoint
-class PlaceRegisterFragment : BaseFragment<com.depromeet.baton.databinding.FragmentPlaceRegisterBinding>(R.layout.fragment_place_register) {
+class PlaceRegisterFragment : BaseFragment<FragmentPlaceRegisterBinding>(R.layout.fragment_place_register) {
     private val writePostViewModel: WritePostViewModel by activityViewModels()
     private lateinit var photoRvAdapter: PhotoRvAdapter
 
-    @SuppressLint("ResourceAsColor")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.placeRegisterFragment=this
-        binding.writePostViewModel=writePostViewModel
-        setSearchBar()
+        binding.placeRegisterFragment = this
+        binding.writePostViewModel = writePostViewModel
+
+        setInitLayout()
         selectedShopObserve()
         setPlaceRegisterClickListener()
         setPictureSelectClickListener()
         setPhotoRvAdapter()
         setHashTagClickListener()
-        setSelectedPhtoObserve()
+        setSelectedPhotoObserve()
     }
 
-    //SearchBar 초기 상태 세팅
-    private fun setSearchBar() {
+    //SearchBar 초기 레이아웃 상태 세팅
+    private fun setInitLayout() {
+        with(binding.includeChip) {
+            tvHashtagSelectExplain.text = "(선택사항)"
+            tvHashtagSelectExplain.setTextColor(ContextCompat.getColor(requireContext(), com.depromeet.bds.R.color.gy60))
+            tvHashtagSelect.setTextColor(ContextCompat.getColor(requireContext(), com.depromeet.bds.R.color.gy60))
+        }
+
         with(binding.includeBdsSearchbarOne) {
             searchBarEt.text = Editable.Factory.getInstance().newEditable("헬스장 이름이나 도로명 주소를 검색해주세요")
             searchBarEt.isFocusable = false
-            searchBarEt.setTextColor(Color.GRAY)
+            searchBarEt.setTextColor(ContextCompat.getColor(requireContext(), com.depromeet.bds.R.color.gy60))
             searchBarCancelIc.visibility = View.GONE
         }
         with(binding.includeBdsSearchbarTwo) {
             searchBarEt.isFocusable = false
-            searchBarEt.setTextColor(Color.GRAY)
+            searchBarEt.setTextColor(ContextCompat.getColor(requireContext(), com.depromeet.bds.R.color.gy60))
             searchBarCancelIc.visibility = View.GONE
         }
     }
@@ -73,29 +73,27 @@ class PlaceRegisterFragment : BaseFragment<com.depromeet.baton.databinding.Fragm
 
     //TODO textField Bds 적용
     private fun selectedShopObserve() {
-        writePostViewModel.selectedShopInfo.observe(this) { selectedShopInfo ->
+        writePostViewModel.selectedShopInfo.observe(viewLifecycleOwner) { selectedShopInfo ->
             binding.tvPlaceRegister.text = "이름"
-            binding.tvPlaceRegisterExplain.text = "주소"
+            binding.tvPlaceRegisterAddress.visibility = View.VISIBLE
 
             with(binding.includeBdsSearchbarOne) {
                 searchBarSearchIc.visibility = View.GONE
                 ctlSearchBarContainer.setBackgroundResource(com.depromeet.bds.R.drawable.temp_bg_search_bar)
                 searchBarEt.text = Editable.Factory.getInstance().newEditable(selectedShopInfo.shopName)
+                searchBarEt.isEnabled = false
             }
             with(binding.includeBdsSearchbarTwo) {
                 ctlSearchBarContainer.visibility = View.VISIBLE
-                searchBarEt.text = Editable.Factory.getInstance().newEditable(selectedShopInfo.shopAddress)
                 ctlSearchBarContainer.setBackgroundResource(com.depromeet.bds.R.drawable.temp_bg_search_bar)
                 searchBarSearchIc.visibility = View.GONE
-
+                searchBarEt.text = Editable.Factory.getInstance().newEditable(selectedShopInfo.shopAddress)
             }
         }
     }
 
-    //TODO 해시태그 선택 adapter로 구현
+    //TODO 해시태그 선택 bindingAdapter로 구현 + ViewModel로 옮기기
     private fun setHashTagClickListener() {
-        binding.includeChip.tvHashtagSelectExplain.text = "(선택사항)"
-
         with(binding.includeChip) {
             bdschoiceHashtagKindTeacher.setOnClickListener {
                 bdschoiceHashtagKindTeacher.setOnAndShape(true, 0)
@@ -121,28 +119,18 @@ class PlaceRegisterFragment : BaseFragment<com.depromeet.baton.databinding.Fragm
         }
     }
 
-    //TODO 1.선택하기 누르면
     private fun setPictureSelectClickListener() {
         binding.ibtnPlaceRegister.setOnClickListener {
-            //TODO 2.권한 체크
             checkPermission()
         }
     }
 
     private fun checkPermission() {
-        val cameraPermission =
+        val galleryPermission =
             ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
-        if (cameraPermission == PackageManager.PERMISSION_GRANTED) {
-            //프로그램 진행
-            Timber.e("접근권한 있음")
-            startProcess()
-        } else {
-            //권한요청
-            Timber.e("접근권한 없음")
-            requestPermission()
-        }
+        if (galleryPermission == PackageManager.PERMISSION_GRANTED) startProcess()
+        else requestPermission()
     }
-
 
     private fun startProcess() {
         val config = ImagePickerConfig(
@@ -158,8 +146,6 @@ class PlaceRegisterFragment : BaseFragment<com.depromeet.baton.databinding.Fragm
             doneTitle = "확인",
             isShowNumberIndicator = true,
             maxSize = 5,
-            folderTitle = "사진 등록",
-            limitMessage = "ㅋㅋㅋ"
         )
         launcher.launch(config)
     }
@@ -170,19 +156,6 @@ class PlaceRegisterFragment : BaseFragment<com.depromeet.baton.databinding.Fragm
         }
     }
 
-    private fun setPhotoRvAdapter() {
-        photoRvAdapter = PhotoRvAdapter(requireContext())
-        binding.rvPlaceRegister.adapter = photoRvAdapter
-    }
-
-    private fun setSelectedPhtoObserve() {
-        writePostViewModel.selectedPhotoList.observe(viewLifecycleOwner) {
-            photoRvAdapter.submitList(it)
-        }
-    }
-
-
-    //TODO 5.권한 없으면 받아오기
     private fun requestPermission() {
         permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
     }
@@ -191,8 +164,20 @@ class PlaceRegisterFragment : BaseFragment<com.depromeet.baton.databinding.Fragm
         registerForActivityResult(ActivityResultContracts.RequestPermission())
         { isGranted: Boolean ->
             when (isGranted) {
-                true -> startProcess() // 권한이 있다면 진행
-                false -> Toast.makeText(requireContext(), "갤러리 권한을 허용해주세요.", Toast.LENGTH_SHORT).show()
+                true -> startProcess()
+                false -> requireContext().shortToast("갤러리 권한을 허용해주세요.")
             }
         }
+
+    private fun setPhotoRvAdapter() {
+        photoRvAdapter = PhotoRvAdapter(requireContext())
+        binding.rvPlaceRegister.adapter = photoRvAdapter
+    }
+
+    //TODO 사진 재 선택시 로직
+    private fun setSelectedPhotoObserve() {
+        writePostViewModel.selectedPhotoList.observe(viewLifecycleOwner) {
+            photoRvAdapter.submitList(it)
+        }
+    }
 }
