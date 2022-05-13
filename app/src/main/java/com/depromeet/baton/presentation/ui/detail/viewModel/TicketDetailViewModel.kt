79800d43a015e9138b04044f9cda3235c06815de
HyomK <qet557@gmail.com>
2022-05-13 15:07:02 +0900

    viewmodel 적용

--- app/src/main/java/com/depromeet/baton/presentation/ui/detail/viewModel/TicketDetailViewModel.kt
+++ app/src/main/java/com/depromeet/baton/presentation/ui/detail/viewModel/TicketDetailViewModel.kt
@@ -23,49 +23,44 @@ class TicketDetailViewModel @Inject constructor() : BaseViewModel() {
     val _uiState = MutableLiveData<UIState?>(UIState.HasData)
     val uiState: LiveData<UIState?> get() = _uiState
 
-    val sellerData = Seller("바통","바통","","2022.04.20")
-
-    val _marketInfoState= MutableLiveData<MarketBasicInfo>( MarketBasicInfo(
-        sellerData,
-        gymName = "휴메이크 휘트니스 석촌점",
-        price = "200,000d원",
-        canSuggest = true,
-        registration_date = "22.05.02"+"가입",
-        views = "260",
-        likes="30",
-        url = "",
-        detailContent = "헬스 회원권  60일권  200,000원으로 저렴하게 양도합니다!\u2028- 접근성 좋음 주변에 버스터미널, 정류장 등 가까움\n" +
-                "- 선생님도 친절/대회 수상이력 당연O, 몸 컨디션 체크 등등 설명도 정말 잘해주세요!!\n" +
-                "- PT시간은 4시이후, 저녁시간대 가능",
-        transactionMethod = TransactionMethod.FACE,
-        moreTag = arrayListOf(AdditionalOptions.BARGAINING, AdditionalOptions.HOLDING)
-
-    )
-    )
+    val _marketInfoState= MutableLiveData<MarketBasicInfo>()
     val marketInfoState : LiveData<MarketBasicInfo> get() = _marketInfoState
 
-
-    val _ticketState= MutableLiveData<TicketInfo>(  TicketInfo(
-        ticketKind = TicketKind.GYM,
-        effectiveDate = "22.12.31",
-        price = "200,000원",
-        transferCoast = "10,000원",
-        infoTag =  arrayListOf(HashTag.SYSTEMATIC_LESSON,HashTag.CUSTOMIZED_CARE, HashTag.QUIET_ATMOSPHERE)
-
-    ))
+    val _ticketState= MutableLiveData<TicketInfo>( )
     val ticketState : LiveData<TicketInfo> get() = _ticketState
 
     init {
+        val sellerData = Seller("바통","바통","","2022.04.20")
 
+        _marketInfoState.value =MarketBasicInfo(
+            sellerData,
+            gymName = "휴메이크 휘트니스 석촌점",
+            price = "200,000d원",
+            canSuggest = true,
+            registration_date = "22.05.02"+"가입",
+            views = "260",
+            likes="30",
+            url = "",
+            detailContent = "헬스 회원권  60일권  200,000원으로 저렴하게 양도합니다!\u2028- 접근성 좋음 주변에 버스터미널, 정류장 등 가까움\n" +
+                    "- 선생님도 친절/대회 수상이력 당연O, 몸 컨디션 체크 등등 설명도 정말 잘해주세요!!\n" +
+                    "- PT시간은 4시이후, 저녁시간대 가능",
+            transactionMethod = TransactionMethod.FACE,
+            moreTag = arrayListOf(AdditionalOptions.BARGAINING, AdditionalOptions.HOLDING)
 
+        )
 
+        _ticketState.value=TicketInfo(
+            ticketKind = TicketKind.GYM,
+            effectiveDate = "22.12.31",
+            price = "200,000원",
+            transferCoast = "10,000원",
+            infoTag =  arrayListOf(HashTag.SYSTEMATIC_LESSON,HashTag.CUSTOMIZED_CARE, HashTag.QUIET_ATMOSPHERE)
 
-
+        )
     }
 
    //dummy data
     fun loadData(){
-      // _uiState.value = UIState.HasData  // network 작업 종료
 
     }
 
