cc5b078aea74eeb7a66eddd78fd3dc046c1af7a1
HyomK <qet557@gmail.com>
2022-05-13 23:56:33 +0900

    solved conflict

--- app/src/main/java/com/depromeet/baton/presentation/util/BindingAdapters.kt
+++ app/src/main/java/com/depromeet/baton/presentation/util/BindingAdapters.kt
@@ -3,7 +3,6 @@ package com.depromeet.baton.presentation.util
 import android.widget.ProgressBar
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.depromeet.bds.component.BdsActionChip
 import com.depromeet.bds.component.BdsChoiceChip
 import com.depromeet.bds.component.BdsFilter
 
@@ -17,10 +16,6 @@ fun setBdsFilterChip(view: BdsFilter, text: String, isSelected: Boolean) {
 fun setBdsChoiceChip(view: BdsChoiceChip, isOn: Boolean, shape: String) {
     if (shape == "outlined") view.setOnAndShape(isOn, 0)
     else view.setOnAndShape(isOn, 1)
-}
-@BindingAdapter("itemDecoration")
-fun RecyclerView.bindItemDecoration(itemDecoration: RecyclerView.ItemDecoration) {
-    addItemDecoration(itemDecoration)
 }
 
 @BindingAdapter("progress")
