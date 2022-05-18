bd47975f3f72084819f35780e12de159f1df6223
Hyomk <qet557@gmail.com>
2022-05-18 23:55:07 +0900

    SearchItem template으로 변경

--- map_module/src/main/java/com/depromeet/baton/map/domain/entity/SearchAddressItemEntity.kt
+++ map_module/src/main/java/com/depromeet/baton/map/domain/entity/SearchAddressItemEntity.kt
@@ -1,11 +1,10 @@
 package com.depromeet.baton.map.domain.entity
 
-import com.depromeet.baton.map.data.model.SearchAddressModel
-
-class SearchItemEntity(_item : List<AddressEntity>) {
+class SearchAddressItemEntity(_item : List<AddressEntity>) {
     val item = _item
     fun mapToUi()
         = ArrayList<AddressEntity>().apply{
             this.addAll(item)
         }
 }
+
