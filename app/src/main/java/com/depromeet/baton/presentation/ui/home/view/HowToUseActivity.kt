a5b1294a6d2a6d84acb63ad06fcb8d0703006669
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-06-01 18:22:06 +0900

    no message

---
+++ app/src/main/java/com/depromeet/baton/presentation/ui/home/view/HowToUseActivity.kt
@@ -0,0 +1,14 @@
+package com.depromeet.baton.presentation.ui.home.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import com.depromeet.baton.R
+import com.depromeet.baton.databinding.ActivityWritePostBinding
+import com.depromeet.baton.presentation.base.BaseActivity
+import com.depromeet.baton.presentation.ui.writepost.viewmodel.WritePostViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HowToUseActivity : BaseActivity<ActivityHowToUseBinding>(R.layout.activity_how_to_use) {
+}
