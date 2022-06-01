9ce9f6d2f61bee9adee665e13ff909bff5c5df86
Hyomk <qet557@gmail.com>
2022-06-01 17:21:08 +0900

    MaxDistance 수정

--- app/src/main/java/com/depromeet/baton/util/MaxDistance.kt
+++ app/src/main/java/com/depromeet/baton/util/MaxDistance.kt
@@ -1,20 +1,18 @@
 package com.depromeet.baton.util
 
-class MaxDistance( _distance : Int ){
-    val distance = _distance  //m로 변환된 값
+class MaxDistance( private val distance : Int ){
+    //m로 변환된 값
     val unit = if(distance>=1000) "km" else "m"
 
-    fun getKM() :Float{
-        return if(distance>=1000)distance.toFloat() /1000.0f
-        else distance.toFloat()
+    fun getDistanceToKM() : Float{
+        return distance.toFloat() /1000.0f
     }
 
-    fun getM() : Int = distance
+    fun getDistance() : Int = distance
 
     fun getMaxDistanceWithUnit() : String {
         return if(unit=="km")
             String.format("%1.1f",(distance.toFloat()/1000.0f))+unit
         else distance.toString()+unit
     } // unit을 고려해 출력 ex) 1.3km , 900m
-
 }
