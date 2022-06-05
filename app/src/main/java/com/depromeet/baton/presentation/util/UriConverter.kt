671cb9b2d5123376d2b9dfb8de0f1980ea8d0985
Hyomk <qet557@gmail.com>
2022-06-06 04:57:44 +0900

    uriConverter

---
+++ app/src/main/java/com/depromeet/baton/presentation/util/UriConverter.kt
@@ -0,0 +1,15 @@
+package com.depromeet.baton.presentation.util
+
+import android.content.ContentResolver
+import android.content.Context
+import android.net.Uri
+
+fun uriConverter(context: Context, resourceId: Int): Uri {
+    val uriParser= Uri.Builder()
+        .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+        .authority(context.resources.getResourcePackageName(resourceId))
+        .appendPath(context.resources.getResourceTypeName(resourceId))
+        .appendPath(context.resources.getResourceEntryName(resourceId))
+        .build()
+    return uriParser
+}
