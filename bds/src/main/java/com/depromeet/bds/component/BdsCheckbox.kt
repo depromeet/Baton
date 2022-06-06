0d19eede0abf8f8dfab3531ced9ee59b1c0b159a
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-06-06 15:56:34 +0900

    chip bds로 변경

---
+++ bds/src/main/java/com/depromeet/bds/component/BdsCheckbox.kt
@@ -0,0 +1,40 @@
+package com.depromeet.bds.component
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import androidx.core.content.withStyledAttributes
+import com.depromeet.bds.R
+import com.depromeet.bds.databinding.BdsComponentCheckboxBinding
+
+class BdsCheckbox @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0,
+) : LinearLayout(context, attrs, defStyleAttr, defStyleRes) {
+
+    private val binding: BdsComponentCheckboxBinding
+
+    init {
+
+        val layoutInflater = LayoutInflater.from(context)
+        binding = BdsComponentCheckboxBinding.inflate(layoutInflater, this, true)
+
+        context.withStyledAttributes(attrs, R.styleable.BdsToggle) {
+            binding.checkbox.isChecked = getBoolean(R.styleable.BdsCheckbox_isChecked, false)
+            binding.checkbox.isEnabled = getBoolean(R.styleable.BdsCheckbox_isEnabled, true)
+        }
+    }
+
+    fun setOnClickListener(listener: () -> Unit) {
+        binding.checkbox.setOnClickListener { listener() }
+    }
+
+    fun getIsChecked() = binding.checkbox.isChecked
+
+    fun setIsEnabled(isEnabled: Boolean) {
+        binding.checkbox.isEnabled = isEnabled
+    }
+}
