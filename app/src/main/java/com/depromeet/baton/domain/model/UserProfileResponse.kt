a2154decfba07a7b9d3c0b876550ecd9fe9c2f08
Hyomk <qet557@gmail.com>
2022-06-08 19:38:36 +0900

    User Response 추가

---
+++ app/src/main/java/com/depromeet/baton/domain/model/UserProfileResponse.kt
@@ -0,0 +1,18 @@
+package com.depromeet.baton.domain.model
+
+import com.squareup.moshi.Json
+
+data class UserProfileResponse(
+    @Json(name = "id") val id: Int,
+    @Json(name = "name") val name: String,
+    @Json(name = "phone_number") val phone_number: String,
+    @Json(name = "created_on") val created_on : String,
+    @Json(name = "account") val account: Account
+){
+    data class Account(
+        @Json(name = "holder") val holder: String ,
+        @Json(name = "bank") val bank: String ,
+        @Json(name = "number") val number: String ,
+    )
+}
+
