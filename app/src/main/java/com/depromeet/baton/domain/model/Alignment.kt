c5fb3dce67e1dd90153eb45c6d7b095c687bb576
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-06-12 19:08:19 +0900

    '남은 횟수 많은 순' 정렬 기준 추가 반영

--- app/src/main/java/com/depromeet/baton/domain/model/Alignment.kt
+++ app/src/main/java/com/depromeet/baton/domain/model/Alignment.kt
@@ -1,8 +1,9 @@
 package com.depromeet.baton.domain.model
 
 enum class Alignment(val value: String) {
-    DISTANCE("가까운 거리순"),
-    LOWER_PRICE("낮은 가격순"),
-    VIEW("인기순"),
-    REMAIN_DAY("남은 기간 많은순")
+    DISTANCE("가까운 거리 순"),
+    LOWER_PRICE("낮은 가격 순"),
+    VIEWS("인기 순"),
+    REMAIN_DAY("남은 기간 많은 순"),
+    REMAIN_NUMBER("남은 횟수 많은 순")
 }
