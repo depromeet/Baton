feda33c5966180a28d0fa5c79121cfcbe6cffd4f
Hyomk <qet557@gmail.com>
2022-06-12 22:21:21 +0900

    bookmark api 연결

---
+++ app/src/main/java/com/depromeet/baton/domain/api/ticket/BookmarkApi.kt
@@ -0,0 +1,22 @@
+package com.depromeet.baton.domain.api.ticket
+
+import com.depromeet.baton.remote.ticket.BookmarkRequest
+import com.depromeet.baton.remote.ticket.BookmarkResponse
+import com.depromeet.baton.remote.ticket.BookmarkService
+import retrofit2.Response
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class BookmarkApi  @Inject constructor(
+    private val service : BookmarkService
+){
+
+    suspend fun postBookmark( userIdx : Int , ticketId : Int):Response<BookmarkResponse>{
+        return service.postBookmark(BookmarkRequest(userIdx,ticketId))
+    }
+
+    suspend fun deleteBookmark(bookmarkId: Int) : Response<String>{
+        return service.deleteBookmark(bookmarkId)
+    }
+}
---
+++ app/src/main/java/com/depromeet/baton/remote/ticket/BookmarkService.kt
@@ -0,0 +1,33 @@
+package com.depromeet.baton.remote.ticket
+
+import com.depromeet.baton.domain.model.LoginKakaoRequest
+import com.depromeet.baton.domain.model.LoginKakaoResponse
+import com.squareup.moshi.Json
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.POST
+import retrofit2.http.Path
+import java.net.IDN
+
+interface  BookmarkService {
+    @POST("bookmarks")
+    suspend fun postBookmark(
+        @Body request : BookmarkRequest
+    ) : Response<BookmarkResponse>
+
+    @DELETE("bookmarks/{id}")
+    suspend fun deleteBookmark(
+        @Path("id") bookmarkId : Int
+    ) : Response<String>
+}
+
+data class BookmarkRequest(
+    @Json(name="user") val user : Int,
+    @Json(name="ticket") val ticket : Int
+)
+
+data class BookmarkResponse(
+    @Json(name="user") val user : Int,
+    @Json(name="ticket") val ticket : Int
+)
