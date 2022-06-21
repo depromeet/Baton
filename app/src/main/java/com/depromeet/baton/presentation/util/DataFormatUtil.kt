2917f346b0ba152a35e387c336fe380a26f8d8cc
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-06-22 01:10:12 +0900

    거리 km 반영

--- app/src/main/java/com/depromeet/baton/presentation/util/DataFormatUtil.kt
+++ app/src/main/java/com/depromeet/baton/presentation/util/DataFormatUtil.kt
@@ -22,7 +22,7 @@ fun priceFormatWithZero(price: Float): String {
     val dec = DecimalFormat("#,###")
 
     return if (priceStr.length >= 5)
-        dec.format((priceStr.substring(0, priceStr.length - 4) + "0000").toInt())
+        dec.format((priceStr.substring(0, priceStr.length - 3) + "000").toInt())
     else "0"
 }
 
