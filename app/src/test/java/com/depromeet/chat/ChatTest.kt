e61be1bee6b71eeb728cff93562f6502a51c3f3b
greedy0110 <greedy0110@gmail.com>
2022-06-21 22:57:06 +0900

    ChatTest disable

--- app/src/test/java/com/depromeet/chat/ChatTest.kt
+++ app/src/test/java/com/depromeet/chat/ChatTest.kt
@@ -25,7 +25,8 @@ class MemoryRealTimeDataPublisher : RealTimeDataPublisher {
 }
 
 class ChatTest : FeatureSpec({
-    feature("채팅 기능") {
+    // 단순 코드 시연용이라서 ignore 한다.
+    feature("채팅 기능").config(enabled = false) {
         val scope = CoroutineScope(Dispatchers.Unconfined)
 
         scenario("메시지를 보낸다.") {
