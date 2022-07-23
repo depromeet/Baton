236974fd2a72c62ea9067cb276ce5dac89622d09
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-07-23 17:19:30 +0900

    FirebaseMessagingService 세팅

--- app/src/main/java/com/depromeet/baton/FirebaseMessagingService.kt
+++ app/src/main/java/com/depromeet/baton/FirebaseMessagingService.kt
@@ -1,4 +1,4 @@
-package com.depromeet.baton
+package org.cardna
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
@@ -7,59 +7,76 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
-import com.depromeet.baton.presentation.sample.SampleActivity
+import com.depromeet.baton.presentation.main.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import timber.log.Timber
 
+
 class FirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
-        Timber.d("new Token: $token")
+        Timber.e("new Token: $token")
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
-        Timber.d("From: " + remoteMessage.data)
 
-        val title = remoteMessage.notification?.title
-        val message = remoteMessage.notification?.body
+        Timber.e("From: " + remoteMessage.data)
 
-        if (!message.isNullOrEmpty()) {
-            Timber.d("notice 타이틀: $title")
-            Timber.d("notice 바디: $message")
-            sendNotiNotification(remoteMessage)
+        if (remoteMessage.data.isNotEmpty() ) {
+            sendNotification(remoteMessage)
         } else {
-            Timber.e("notice 수신에러: data가 비어있습니다. 메시지를 수신하지 못했습니다.")
+            Timber.d("pushAlarm", "메시지를 수신하지 못했습니다.")
         }
+
+
     }
 
-    private fun sendNotiNotification(remoteMessage: RemoteMessage) {
+    private fun sendNotification(remoteMessage: RemoteMessage) {
         val uniId = remoteMessage.sentTime.toInt()
-        val intent = Intent(this, SampleActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        //todo 문의하기 Activity로 변경
+        val intent = Intent(this, MainActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        }
+
+        for (key in remoteMessage.data.keys) {
+            intent.putExtra(key, remoteMessage.data.getValue(key))
+        }
 
         val pendingIntent = PendingIntent.getActivity(
-            this, uniId, intent, PendingIntent.FLAG_MUTABLE)
+            this, uniId, intent, PendingIntent.FLAG_MUTABLE
+        )
 
         val channelId = "노티피케이션 메시지"
 
         val notificationBuilder =
             NotificationCompat.Builder(this, channelId)
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle(remoteMessage.notification?.body.toString())
-                .setContentText(remoteMessage.notification?.title.toString())
+          //      .setSmallIcon(R.mipmap.ic_cardna_logo)
+          //      .setLargeIcon((BitmapFactory.decodeResource(resources, R.drawable.img_logo)))
+                .setContentTitle("바통")
+                .setContentText(remoteMessage.data["body"].toString())
                 .setAutoCancel(true)
                 .setContentIntent(pendingIntent)
+                .setSound(null) //소리
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC) //잠금
 
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel =
-                NotificationChannel(channelId, "Notice", NotificationManager.IMPORTANCE_DEFAULT)
+                NotificationChannel(
+                    channelId, "Notice",
+                    NotificationManager.IMPORTANCE_DEFAULT,
+                )
+            channel.setShowBadge(true)
             notificationManager.createNotificationChannel(channel)
         }
+
         notificationManager.notify(uniId, notificationBuilder.build())
     }
 }
