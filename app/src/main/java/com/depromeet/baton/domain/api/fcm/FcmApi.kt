30a960e1afa2c816f342a2c96c513005a70bedf0
Hyomk <qet557@gmail.com>
2022-07-24 23:56:33 +0900

    API 연결

---
+++ app/src/main/java/com/depromeet/baton/domain/api/fcm/FcmApi.kt
@@ -0,0 +1,15 @@
+package com.depromeet.baton.domain.api
+
+
+import com.depromeet.baton.remote.fcm.FcmData
+import com.depromeet.baton.remote.fcm.FcmService
+import retrofit2.Response
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class FcmApi @Inject constructor(private val service :FcmService) {
+    suspend fun postFcmData(token :String, title: String, body: String) : Response<String> {
+        return service.postFcmData(FcmData(token, title, body))
+    }
+}
---
+++ app/src/main/java/com/depromeet/baton/remote/fcm/FcmService.kt
@@ -0,0 +1,20 @@
+package com.depromeet.baton.remote.fcm
+import androidx.annotation.Keep
+import com.squareup.moshi.Json
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface FcmService {
+    @POST("/fcm")
+    suspend fun postFcmData(
+        @Body body : FcmData
+    ) : Response<String>
+}
+
+@Keep
+data class FcmData(
+    @Json(name = "targetToken") val token : String,
+    @Json(name="title")val title : String,
+    @Json(name="body")val body : String
+)
